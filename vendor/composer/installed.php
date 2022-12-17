c52eb8a9ecc0c5bf52b1da92f9cdb8ea09a7e300
Krishna K Tiwari <krishna.itdeveloper@gmail.com>
2022-12-17 13:22:31 +0530

    Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ae1ac0f23c28a80b3ed93f217aa67210704a620b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ae1ac0f23c28a80b3ed93f217aa67210704a620b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.6.1',
+            'version' => '6.6.1.0',
+            'reference' => 'a336b531f6f6b5487fca0caf034a671d4e60df5c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
